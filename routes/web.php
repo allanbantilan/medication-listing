5a5ecaccec8b1cf1b72dd2021d973dbc5db38d21
Allan Bantilan <allanbantilan11@gmail.com>
2024-08-07 13:12:24 +0800

    adde

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Livewire\ListMedications;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', ListMedications::class);
